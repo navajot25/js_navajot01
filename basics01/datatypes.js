162b71c9f05f23f99cbd0b93985a0276ecc7335f
navajot25 <navajotmishra80@gmail.com>
2026-01-09 17:22:39 +0000

    container config

---
+++ basics01/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello"); we are using nodejs,not browser
+
+console.log(3
+    +3);//code readability be high
+
+    console.log("navajot");
+
+let name="sarika";
+let age=19;
+let isLoggedIn=false;
+let state;
+
+// number=>2 to power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique 
+
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
